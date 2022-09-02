2b7327c42da84b78181f96fc097293cc343dbd91
ELBEQQAL94 <elbeqqal.youssef@gmail.com>
2022-09-02 12:21:33 +0200

    first commit

--- src/main/kotlin/de/imedia24/shop/controller/ProductController.kt
+++ src/main/kotlin/de/imedia24/shop/controller/ProductController.kt
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import javax.websocket.server.PathParam
 
@@ -14,7 +15,7 @@ class ProductController(private val productService: ProductService) {
 
     private val logger = LoggerFactory.getLogger(ProductController::class.java)!!
 
-    @GetMapping("/products/{sku}", produces = ["application/json;charset=utf-8"])
+    @GetMapping("product/{sku}", produces = ["application/json;charset=utf-8"])
     fun findProductsBySku(
         @PathVariable("sku") sku: String
     ): ResponseEntity<ProductResponse> {
--- src/main/kotlin/de/imedia24/shop/service/ProductService.kt
+++ src/main/kotlin/de/imedia24/shop/service/ProductService.kt
@@ -1,13 +1,15 @@
 package de.imedia24.shop.service
 
+import de.imedia24.shop.db.entity.ProductEntity
 import de.imedia24.shop.db.repository.ProductRepository
 import de.imedia24.shop.domain.product.ProductResponse
+import de.imedia24.shop.domain.product.ProductResponse.Companion.toProductResponse
 import org.springframework.stereotype.Service
 
 @Service
 class ProductService(private val productRepository: ProductRepository) {
 
     fun findProductBySku(sku: String): ProductResponse? {
-        TODO("to be implemented")
+        return productRepository.findBySku(sku)?.toProductResponse();
     }
 }
